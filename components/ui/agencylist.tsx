b1e58f73fafb099c4b8891275119e7465b9db711
Zack Ren <renzackary@gmail.com>
2024-09-15 19:19:42 -0400

    clean up code

--- components/ui/agencylist.tsx
+++ components/ui/agencylist.tsx
@@ -1,84 +1,105 @@
-import Image from "next/image";
 import React from "react";
+import Image from "next/image";
 import {
-    Card,
-    CardBadge,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
+  Card,
+  CardBadge,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
 import { CardLinks } from "@/components/ui/cardlinks"
 import { CardInline } from "@/components/ui/cardinline"
-import { AgencyLogo } from "@/components/ui/agencylogo"
+
+const agencies = [
+  {
+    title: "Turtle",
+    location: "Vancouver, British Columbia",
+    employees: "51-200",
+    services: [
+      "UX/UI",
+      "Web"
+    ],
+    logo: "/images/turtledesign_logo.jpg",
+    alt: "Image of Turtle logo.",
+    content: "We design world-class teams that design world-class products",
+    link1: "https://www.linkedin.com/company/turtledesign",
+    link2: "https://www.turtle.design/"
+  },
+  {
+    title: "Daybreak Studio",
+    location: "Toronto, Ontario",
+    employees: "11-50",
+    services: [
+      "Brand",
+      "Web",
+      "Software"
+    ],
+    logo: "/images/daybreakstudio_logo.jpg",
+    alt: "Image of Daybreak Studios logo.",
+    content: "A technology first design studio.",
+    link1: "https://www.linkedin.com/company/daybreakstudio",
+    link2: "https://www.daybreak.studio/"
+  },
+  {
+    title: "Green Stone",
+    location: "Toronto, Ontario",
+    employees: "11-50",
+    services: [
+      "UX/UI",
+      "Brand",
+      "Service"
+    ],
+    logo: "/images/green_stone_logo.jpg",
+    alt: "Image of Green Stone logo.",
+    content: "A digital experience agency that delivers customer loyalty through human-centric products, platforms, and journeys.",
+    link1: "https://www.linkedin.com/company/green-stone/",
+    link2: "https://www.greenstone.co/"
+  },
+]
+
+
+
 
 const AgencyList = () => {
-    return (
-        <>
-        <div className="grid grid-cols-3 gap-4">
-        <Card>
-            <CardHeader>
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-4">
+        {
+          agencies.map((agency: any, i: number) => (
+            <Card key={i}>
+              <CardHeader>
                 <CardInline>
-                <div>
-                <CardTitle>Turtle</CardTitle>
-                <CardDescription>Vancouver, British Columbia</CardDescription>
-                <CardDescription>51-200</CardDescription>
-                <CardBadge><Badge>UX/UI</Badge><Badge>Web</Badge></CardBadge>
-                </div>
-                <AgencyLogo src="/images/turtledesign_logo.jpg" alt="Image of Turtle logo."></AgencyLogo>
+                  <div>
+                    <CardTitle>{agency.title}</CardTitle>
+                    <CardDescription>{agency.location}</CardDescription>
+                    <CardDescription>{agency.employees}</CardDescription>
+                    {agency.services.map((service: string, j: number) => (
+                      <CardBadge key={j}>
+                        <Badge>
+                          {service}
+                        </Badge>
+                      </CardBadge>
+                    ))}
+                  </div>
+                  <Image src={agency.logo} alt={agency.alt} width={50} height={50} className="ml-auto h-full w-auto object-contain rounded"></Image>
                 </CardInline>
-            </CardHeader>
-            <CardContent>
-                <p>We design world-class teams that design world-class products.</p>
-            </CardContent>
-            <CardFooter>
-                <CardLinks href1="https://www.linkedin.com/company/turtledesign/" href2="https://www.turtle.design/"></CardLinks>
-            </CardFooter>
-        </Card>
-        <Card>
-            <CardHeader>
-                <CardInline>
-                <div>
-                <CardTitle>Daybreak Studio</CardTitle>
-                <CardDescription>Toronto, Ontario</CardDescription>
-                <CardDescription>11-50</CardDescription>
-                <CardBadge><Badge>Brand</Badge><Badge>Web</Badge><Badge>Software</Badge></CardBadge>
-                </div>
-                <AgencyLogo src="/images/daybreakstudio_logo.jpg" alt="Image of Daybreak Studios logo."></AgencyLogo>
-                </CardInline>
-            </CardHeader>
-            <CardContent>
-                <p>A technology first design studio.</p>
-            </CardContent>
-            <CardFooter>
-                <CardLinks href1="https://www.linkedin.com/company/daybreakstudio/" href2="https://www.daybreak.studio/"></CardLinks>
-            </CardFooter>
-        </Card>
-        <Card>
-            <CardHeader>
-                <CardInline>
-                <div>
-                <CardTitle>Green Stone</CardTitle>
-                <CardDescription>Toronto, Ontario</CardDescription>
-                <CardDescription>11-50</CardDescription>
-                <CardBadge><Badge>UX/UI</Badge><Badge>Brand</Badge><Badge>Service</Badge></CardBadge>
-                </div>
-                <AgencyLogo src="/images/green_stone_logo.jpg" alt="Image of Green Stone logo."></AgencyLogo>
-                </CardInline>
-            </CardHeader>
-            <CardContent>
-                <p>A digital experience agency that delivers customer loyalty through human-centric products, platforms, and journeys.</p>
-            </CardContent>
-            <CardFooter>
-                <CardLinks href1="https://www.linkedin.com/company/green-stone/" href2="https://www.greenstone.co//"></CardLinks>
-            </CardFooter>
-        </Card>
-        </div>
-        </>
-      );
-    
+              </CardHeader>
+              <CardContent>
+                <p>{agency.content}</p>
+              </CardContent>
+              <CardFooter>
+                <CardLinks href1={agency.link1} href2={agency.link2} />
+              </CardFooter>
+            </Card>
+          ))
+        }
+      </div>
+    </>
+  );
+
 }
 
-export {AgencyList}
+export { AgencyList }
--- components/ui/cardlinks.tsx
+++ components/ui/cardlinks.tsx
@@ -3,20 +3,20 @@ import React from "react";
 interface CardLinksProps {
     href1: string;
     href2: string;
-  }
+}
 
-const CardLinks : React.FC<CardLinksProps> = ({ href1, href2 }) => {
+const CardLinks: React.FC<CardLinksProps> = ({ href1, href2 }) => {
     return (
-        <>
+        <div className="flex w-full justify-between">
             <a className="underline text-center" href={href1} target="_blank">
                 Linkedin
             </a>
-            <div className="w-full">
-                <div className="inline-flex items-center justify-center float-right whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring bg-primary text-primary-foreground shadow hover:bg-primary/75 h-9 px-4 py-2"><a href={href2} target="_blank">Visit Website</a></div>
-            </div>
-        </>
-      );
-    
+            <a className="rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring bg-primary text-primary-foreground shadow hover:bg-primary/75 h-9 px-4 py-2" href={href2} target="_blank">
+                Visit Website
+            </a>
+        </div>
+    );
+
 }
 
 export { CardLinks }
